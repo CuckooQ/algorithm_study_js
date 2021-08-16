e284ffa0b8c340dd1069dc72dedf7949a9780f7e
jaeyong4536 <jaeyong4536@gmail.com>
2021-08-16 18:54:26 +0900

    섹션2 문제3 수정
    src/s2/s2p3.js
    : 클래스 RockScissorPaperUser의 private프로퍼티의 set, get메소드 추가.
      클래스 RockScissorPaperUser의 private프로퍼티의 set, get메소드를 사용하도록 수정.
      최상단 괄호로 해당 파일의 코드 묶기.

--- src/s2/s2p3.js
+++ src/s2/s2p3.js
@@ -16,182 +16,194 @@
  *                 B
  *                 D
  */
-
-class RockScissorPaperUser {
-    #selection;
+{
+    class RockScissorPaperUser {
+        #selection;
+        
+        constructor(selection){
+            this.setSeleciton(selection);
+        }
     
-    constructor(selection){
-        this.selection = selection;
+        isDraw(opponent) {
+            return this.getSelection() === opponent.getSelection();
+        }
+    
+        isWin(opponent) {
+            const mySelection = this.getSelection();
+            const oppSelection = opponent.getSelection();
+            if (mySelection === this.ROCKSCISSORPAPER.SCISSOR && oppSelection === this.ROCKSCISSORPAPER.PAPER) {
+                return true;
+            } else if (mySelection === this.ROCKSCISSORPAPER.ROCK && oppSelection === this.ROCKSCISSORPAPER.SCISSOR) {
+                return true;
+            } else if(mySelection === this.ROCKSCISSORPAPER.PAPER && oppSelection === this.ROCKSCISSORPAPER.ROCK){
+                return true;
+            } else {
+                return false;
+            }
+        }
+    
+        isLose (opponent) {
+            return !this.isDraw(opponent) && !this.isWin(opponent);
+        }
+    
+        getSelection() {
+            return this.#selection;
+        }
+    
+        setSeleciton(selection) {
+            this.#selection = selection;
+        }
+    
+        ROCKSCISSORPAPER = {
+            SCISSOR: 1,
+            ROCK: 2,
+            PAPER: 3
+        };
     }
-
-    isDraw (opponent) {
-        return this.selection === opponent.selection;
-    }
-
-    isWin (opponent) {
-        if (this.selection === this.ROCKSCISSORPAPER.SCISSOR && opponent.selection === this.ROCKSCISSORPAPER.PAPER) {
-            return true;
-        } else if (this.selection === this.ROCKSCISSORPAPER.ROCK && opponent.selection === this.ROCKSCISSORPAPER.SCISSOR) {
-            return true;
-        } else if(this.selection === this.ROCKSCISSORPAPER.PAPER && opponent.selection === this.ROCKSCISSORPAPER.ROCK){
-            return true;
-        } else {
-            return false;
+    
+    function getResult(aSelection, bSelection) {
+        const PLAYER_A = "A";
+        const PLAYER_B = "B";
+        const DRAW = "D";
+        aUser = new RockScissorPaperUser(aSelection);
+        bUser = new RockScissorPaperUser(bSelection);
+        if (aUser.isDraw(bUser)) {
+            return DRAW;
+        } 
+        if (aUser.isWin(bUser)) {
+            return PLAYER_A;
+        } 
+        if (aUser.isLose(bUser)) {
+            return PLAYER_B;
         }
     }
-
-    isLose (opponent) {
-        return !this.isDraw(opponent) && !this.isWin(opponent);
-    }
-
-    ROCKSCISSORPAPER = {
-        SCISSOR: 1,
-        ROCK: 2,
-        PAPER: 3
-    };
-}
-
-function getResult(aSelection, bSelection) {
-    const PLAYER_A = "A";
-    const PLAYER_B = "B";
-    const DRAW = "D";
-    aUser = new RockScissorPaperUser(aSelection);
-    bUser = new RockScissorPaperUser(bSelection);
-    if (aUser.isDraw(bUser)) {
-        return DRAW;
-    } 
-    if (aUser.isWin(bUser)) {
-        return PLAYER_A;
-    } 
-    if (aUser.isLose(bUser)) {
-        return PLAYER_B;
-    }
-}
-
-function solution (gameCount, aSelections, bSelections) {
-    const results = [];
-    for (let i=0; i<gameCount; i++) {
-        results.push(getResult(aSelections[i], bSelections[i]));
-    }
-    const answer = results.join('\n');
-    return answer;
-}
-
-function testToAwin () {
-    const testNum = 1;
-    const inputCount = 2;
-    const testFunction = solution;
-    let inputAarr = [2, 2];
-    let inputBarr = [1, 1];   
-    let expectResult = "A\nA";
-    let condition = (testFunction(inputCount, inputAarr, inputBarr) === expectResult);    
-    validateTestResult(testNum, condition);
-
-    inputAarr = [3, 3];
-    inputBarr = [2, 2];   
-    expectResult = "A\nA";
-    condition = (testFunction(inputCount, inputAarr, inputBarr) === expectResult);    
-    validateTestResult(testNum, condition);
-
-    inputAarr = [1, 1];
-    inputBarr = [3, 3];   
-    expectResult = "A\nA";
-    condition = (testFunction(inputCount, inputAarr, inputBarr) === expectResult);    
-    validateTestResult(testNum, condition);
-}
-
-function testToAdraw () {
-    const testNum = 2;
-    const inputCount = 2;
-    const testFunction = solution;
-    let inputAarr = [1, 1];
-    let inputBarr = [1, 1];   
-    let expectResult = "D\nD";
-    let condition = (testFunction(inputCount, inputAarr, inputBarr) === expectResult);    
-    validateTestResult(testNum, condition);
-
-    inputAarr = [2, 2];
-    inputBarr = [2, 2];   
-    expectResult = "D\nD";
-    condition = (testFunction(inputCount, inputAarr, inputBarr) === expectResult);    
-    validateTestResult(testNum, condition);
-
-    inputAarr = [3, 3];
-    inputBarr = [3, 3];   
-    expectResult = "D\nD";
-    condition = (testFunction(inputCount, inputAarr, inputBarr) === expectResult);    
-    validateTestResult(testNum, condition);
-}
-
-function testToAlose () {
-    const testNum = 3;
-    const inputCount = 2;
-    const testFunction = solution;
-    let inputAarr = [1, 1];
-    let inputBarr = [2, 2];   
-    let expectResult = "B\nB";
-    let condition = (testFunction(inputCount, inputAarr, inputBarr) === expectResult);    
-    validateTestResult(testNum, condition);
-
-    inputAarr = [2, 2];
-    inputBarr = [3, 3];   
-    expectResult = "B\nB";
-    condition = (testFunction(inputCount, inputAarr, inputBarr) === expectResult);    
-    validateTestResult(testNum, condition);
-
-    inputAarr = [3, 3];
-    inputBarr = [1, 1];   
-    expectResult = "B\nB";
-    condition = (testFunction(inputCount, inputAarr, inputBarr) === expectResult);    
-    validateTestResult(testNum, condition);
-}
-
-function testToMaxGame () {
-    const testNum = 4;
-    const inputCount = 100;
-    const inputAarr = [];
-    const inputBarr = [];
-    const resultArr = [];
-    for (let i=0; i<100;i++) {
-        inputAarr.push(i%3+1);
-        inputBarr.push(i%3+1);
-        resultArr.push("D");
-    }
-    const expectResult = resultArr.join('\n');
-    const testFunction = solution;
-    const condition = (testFunction(inputCount, inputAarr, inputBarr) === expectResult);    
-    validateTestResult(testNum, condition);
-}
-
-function testToMinGame () {
-    const testNum = 5;
-    const inputCount = 1;
-    const inputAarr = [1];
-    const inputBarr = [2];   
-    const expectResult = "B";
-    const testFunction = solution;
-    const condition = (testFunction(inputCount, inputAarr, inputBarr) === expectResult);    
-    validateTestResult(testNum, condition);
-}
-
-function main () {
-    const inputCount = 5;
-    const inputAarr = [2, 3, 3, 1, 3];
-    const inputBarr = [1, 1, 2, 2, 3];
-    const output = this.solution(inputCount, inputAarr, inputBarr);
     
-    console.log("S2P3\n");
-    // test();
-    console.log(`Input: ${inputCount}\n ${inputAarr.join(' ')}\n ${inputBarr.join(' ')}\n`);
-    console.log(`Output: ${output}\n`);
-}
+    function solution(gameCount, aSelections, bSelections) {
+        const results = [];
+        for (let i=0; i<gameCount; i++) {
+            results.push(getResult(aSelections[i], bSelections[i]));
+        }
+        const answer = results.join('\n');
+        return answer;
+    }
+    
+    function testToAwin() {
+        const testNum = 1;
+        const inputCount = 2;
+        const testFunction = solution;
+        let inputAarr = [2, 2];
+        let inputBarr = [1, 1];   
+        let expectResult = "A\nA";
+        let condition = (testFunction(inputCount, inputAarr, inputBarr) === expectResult);    
+        validateTestResult(testNum, condition);
+    
+        inputAarr = [3, 3];
+        inputBarr = [2, 2];   
+        expectResult = "A\nA";
+        condition = (testFunction(inputCount, inputAarr, inputBarr) === expectResult);    
+        validateTestResult(testNum, condition);
+    
+        inputAarr = [1, 1];
+        inputBarr = [3, 3];   
+        expectResult = "A\nA";
+        condition = (testFunction(inputCount, inputAarr, inputBarr) === expectResult);    
+        validateTestResult(testNum, condition);
+    }
+    
+    function testToAdraw() {
+        const testNum = 2;
+        const inputCount = 2;
+        const testFunction = solution;
+        let inputAarr = [1, 1];
+        let inputBarr = [1, 1];   
+        let expectResult = "D\nD";
+        let condition = (testFunction(inputCount, inputAarr, inputBarr) === expectResult);    
+        validateTestResult(testNum, condition);
+    
+        inputAarr = [2, 2];
+        inputBarr = [2, 2];   
+        expectResult = "D\nD";
+        condition = (testFunction(inputCount, inputAarr, inputBarr) === expectResult);    
+        validateTestResult(testNum, condition);
+    
+        inputAarr = [3, 3];
+        inputBarr = [3, 3];   
+        expectResult = "D\nD";
+        condition = (testFunction(inputCount, inputAarr, inputBarr) === expectResult);    
+        validateTestResult(testNum, condition);
+    }
+    
+    function testToAlose() {
+        const testNum = 3;
+        const inputCount = 2;
+        const testFunction = solution;
+        let inputAarr = [1, 1];
+        let inputBarr = [2, 2];   
+        let expectResult = "B\nB";
+        let condition = (testFunction(inputCount, inputAarr, inputBarr) === expectResult);    
+        validateTestResult(testNum, condition);
+    
+        inputAarr = [2, 2];
+        inputBarr = [3, 3];   
+        expectResult = "B\nB";
+        condition = (testFunction(inputCount, inputAarr, inputBarr) === expectResult);    
+        validateTestResult(testNum, condition);
+    
+        inputAarr = [3, 3];
+        inputBarr = [1, 1];   
+        expectResult = "B\nB";
+        condition = (testFunction(inputCount, inputAarr, inputBarr) === expectResult);    
+        validateTestResult(testNum, condition);
+    }
+    
+    function testToMaxGame() {
+        const testNum = 4;
+        const inputCount = 100;
+        const inputAarr = [];
+        const inputBarr = [];
+        const resultArr = [];
+        for (let i=0; i<100;i++) {
+            inputAarr.push(i%3+1);
+            inputBarr.push(i%3+1);
+            resultArr.push("D");
+        }
+        const expectResult = resultArr.join('\n');
+        const testFunction = solution;
+        const condition = (testFunction(inputCount, inputAarr, inputBarr) === expectResult);    
+        validateTestResult(testNum, condition);
+    }
+    
+    function testToMinGame() {
+        const testNum = 5;
+        const inputCount = 1;
+        const inputAarr = [1];
+        const inputBarr = [2];   
+        const expectResult = "B";
+        const testFunction = solution;
+        const condition = (testFunction(inputCount, inputAarr, inputBarr) === expectResult);    
+        validateTestResult(testNum, condition);
+    }
+    
+    function main() {
+        const inputCount = 5;
+        const inputAarr = [2, 3, 3, 1, 3];
+        const inputBarr = [1, 1, 2, 2, 3];
+        const output = this.solution(inputCount, inputAarr, inputBarr);
+        
+        console.log("S2P3\n");
+        // test();
+        console.log(`Input: ${inputCount}\n ${inputAarr.join(' ')}\n ${inputBarr.join(' ')}\n`);
+        console.log(`Output: ${output}\n`);
+    }
+    
+    function test() {
+        testToAwin();
+        testToAdraw();
+        testToAlose();
+        testToMaxGame();
+        testToMinGame();
+    }
+    
+    main();
 
-function test() {
-    testToAwin();
-    testToAdraw();
-    testToAlose();
-    testToMaxGame();
-    testToMinGame();
 }
-
-main();
