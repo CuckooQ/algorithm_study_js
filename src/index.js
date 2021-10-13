ab04b081c9ab7f855d7250b9876abe376bee2109
jaeyong4536 <jaeyong4536@gmail.com>
2021-10-13 12:48:51 +0900

    섹션9 문제7 추가
    review.md
    : 다시 풀 문제에 섹션9 문제7 추가.
    
    src/index.js
    : 섹션9 문제 수 추가.
    
    src/s9/s9p7.js
    : 섹션9 문제7 처리 추가.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@
 
   function loadTests() {
     const directories = ["s1", "s2", "s3", "s4", "s5", "s6", "s7", "s8", "s9"];
-    const fileCounts = [17, 7, 5, 5, 8, 7, 12, 15, 6];
+    const fileCounts = [17, 7, 5, 5, 8, 7, 12, 15, 7];
     directories.forEach((directory, idx) => {
       const fileCount = fileCounts[idx];
       for (let i = 1; i <= fileCount; i++) {
